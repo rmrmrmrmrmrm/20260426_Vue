89910ce4c41948133cac9f573f8f06effd4bf84b
rmrmr <t2g7fcuu@yahoo.co.jp>
2026-04-26 22:44:46 +0900

    vite.config.jsのbase設定変更、npm run buildしてdistの中身を/hogeに複製

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // GitHub Pagesやレンタルサーバーの公開ディレクトリ名に合わせてください
+  base: '/20260426_Vue/hoge/',
 })
