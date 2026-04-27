71aa36eb8cd96f6dbed5e5238b58f335f56b269f
rmrmr <t2g7fcuu@yahoo.co.jp>
2026-04-27 22:36:14 +0900

    パス修正

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <!-- 全ページ共通のナビがあればここに書く -->
-    <router-link to="/hoge/">ホーム（診断一覧）</router-link>
+    <router-link to="/">ホーム（診断一覧）</router-link>
   </nav>
 
   <!-- HomeView.vueで管理 -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
     <h1>診断メニュー</h1>
     <ul>
-        <li><router-link to="/hoge/body">骨格診断</router-link></li>
-        <li><router-link to="/hoge/shoes">靴サイズ</router-link></li>
-        <li><router-link to="/hoge/fortune">今日の占い</router-link></li>
+        <li><router-link to="/body">骨格診断</router-link></li>
+        <li><router-link to="/shoes">靴サイズ</router-link></li>
+        <li><router-link to="/fortune">今日の占い</router-link></li>
     </ul>
 </template>
